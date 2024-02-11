9738e186d546257dff2bee93a93f5d9c40883891
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2024-02-10 22:28:24 -0600

    Advent calendar list

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
+
+const days = Array.from({ length: 24 }, (_, i) => i + 1);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome Ricardo!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        contentContainerStyle={styles.flatList}
+        columnWrapperStyle={styles.column}
+        data={days}
+        renderItem={({ item }: { item: number }) => <DayListItem day={item} />}
+        numColumns={2}
+      />
     </View>
   );
 }
@@ -14,7 +21,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  flatList: {
+    gap: 10,
+    padding: 10
+  },
+  column: {
+    gap: 10,
   },
 });
